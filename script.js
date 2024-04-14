54787c2589341de79f7f356bd5f7cdf0280dfdf3
prxsto <15711032+prxsto@users.noreply.github.com>
2024-04-13 21:50:30 -0700

    resizing pixels logic inmplemented

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ const gridContainer = document.querySelector("#grid-container");
 const newGridButton = document.querySelector("#new-grid-button");
 
 // default values
-let gridContainerSize = "960px";
+let gridContainerSize = 960;
+let pixelNum = 256;
 let pixelSize = "60px";
-let numPixels = 256;
 
-gridContainer.style.width = gridContainerSize;
-gridContainer.style.height = gridContainerSize;
+gridContainer.style.width = gridContainerSize + "px";
+gridContainer.style.height = gridContainerSize + "px";
 
-newGridButton.addEventListener("click", (event) => {
-  newGridSize = prompt("what size grid?", 60);
-  newGridSize = math.min(Number(newGridSize), 100);
+newGridButton.addEventListener("click", (_event) => {
+  let newGridSize = Number(prompt("what size grid?", 16));
+  newGridSize = Math.min(newGridSize, 100);
 
   // destroy current grid, rebuilt using newNumPixels
-  newPixelSize = gridContainer.style.width / newGridSize;
-  newPixelNum = newGridSize * newGridSize;
+  let newPixelNum = newGridSize ** 2;
+  let newPixelSize = Number(gridContainerSize) / newGridSize;
+  newPixelSize = `${newPixelSize}px`;
+  // console.log(`gridContainer: ${gridContainer.style.width}, ${gridContainer.style.height}`);
+  console.log(`newPixelSize: ${newPixelSize}`);
+  console.log(`newPixelNum: ${newPixelNum}`);
   gridContainer.replaceChildren();
-  createPixelGrid(newPixelNum, newPixelSize, false);
-}) 
+  createPixelGrid(newPixelNum, newPixelSize);
+})
 
 function createPixelGrid(numPixels, pixelSize, debug = true) {
   for (let i = 0; i < numPixels; i++) {
@@ -26,6 +30,7 @@ function createPixelGrid(numPixels, pixelSize, debug = true) {
     if (debug) {
       let para = document.createElement("p");
       para.style.textAlign = "center";
+      para.style.fontSize = "2px";
       para.textContent = i + 1;
       pixel.appendChild(para);
     }
@@ -39,4 +44,4 @@ function createPixelGrid(numPixels, pixelSize, debug = true) {
   }
 }
 
-createPixelGrid(numPixels, pixelSize);
+createPixelGrid(pixelNum, pixelSize);
